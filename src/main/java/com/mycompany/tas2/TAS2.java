b22682216ee648ddfd044c6eb2777b6917e17a33
Diaz1401 <diaznura@outlook.com>
2024-02-28 09:49:41 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/tas2/TAS2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tas2;
+
+/**
+ *
+ * @author Diaz
+ */
+public class TAS2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
