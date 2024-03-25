9e17978ab56cc180fbb69bf5c69ed54e504320f1
Diaz1401 <diaznura@outlook.com>
2024-03-25 12:49:48 +0700

    all: Update form style
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- src/Barang/FormBarangPanel.java
+++ src/Barang/FormBarangPanel.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package barang;
+package Barang;
 
 import com.formdev.flatlaf.FlatClientProperties;
 import java.sql.*;
 import java.util.Random;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import koneksi.Koneksi;
+import Koneksi.Koneksi;
 
 /**
  *
---
+++ src/Main/Main.java
@@ -0,0 +1,52 @@
+package Main;
+
+import com.formdev.flatlaf.*;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ *
+ * @author Diaz Nuraji
+ */
+public class Main {
+
+    public static int[] SetSize(double scale) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) (screenSize.getWidth() * scale);
+        int screenHeight = (int) (screenSize.getHeight() * scale);
+        int[] size = {screenWidth, screenHeight};
+        return size;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Setup FlatLaf
+        setupFlatLaf(true);
+        new FormLogin.FormLoginFrame().setVisible(true);
+    }
+
+    private static void setupFlatLaf(boolean light) {
+        // Register custom defaults source
+        FlatLaf.registerCustomDefaultsSource("Theme");
+        try {
+            // Set the look and feel based on the specified theme
+            if (light) {
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            } else {
+                UIManager.setLookAndFeel(new FlatDarkLaf());
+            }
+            // Update the UI to reflect the theme change
+            SwingUtilities.updateComponentTreeUI(new JFrame());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
