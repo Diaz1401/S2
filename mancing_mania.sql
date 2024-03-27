84531633fd4e94e1830aab317b38c3ca552aef6f
Diaz1401 <diaznura@outlook.com>
2024-03-27 09:41:24 +0700

    sql: update
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- mancing_mania.sql
+++ mancing_mania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2024 at 02:56 AM
+-- Generation Time: Mar 27, 2024 at 03:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,12 +39,21 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama`, `harga`, `stok`) VALUES
-('BARANG_12349853', 'Kopi Kapal Api Hitam', 5000, 20),
-('BARANG_29384387', 'Rental Pemancingan', 50000, 9999999),
-('BARANG_44323212', 'Indomie Goreng', 6000, 23),
-('BARANG_79096336', 'Luwak White Coffee', 5000, 20),
-('BARANG_84088033', 'Ikan/Kg', 20000, 9999999),
-('BARANG_90013669', 'Chocolatos Cokelat', 6000, 15);
+('BRG_57654709', 'Indomie Goreng', 6000, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_stok` varchar(100) NOT NULL,
+  `id_barang` varchar(100) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -53,11 +62,19 @@ INSERT INTO `barang` (`id_barang`, `nama`, `harga`, `stok`) VALUES
 --
 
 CREATE TABLE `detail_transaksi` (
-  `id_detail` varchar(100) NOT NULL,
+  `id_detail_transaksi` varchar(100) NOT NULL,
   `id_transaksi` varchar(100) NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  `subtotal` int(11) NOT NULL
+  `id_pegawai` varchar(100) DEFAULT NULL,
+  `tanggal` datetime NOT NULL,
+  `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail_transaksi`, `id_transaksi`, `id_pegawai`, `tanggal`, `total`) VALUES
+('DTRX_61179075', 'TRX_31603246', 'y', '2024-03-27 09:04:49', 12000);
 
 -- --------------------------------------------------------
 
@@ -109,8 +126,7 @@ INSERT INTO `pegawai` (`id_pegawai`, `password`, `nama`, `jabatan`, `gaji`) VALU
 
 CREATE TABLE `transaksi` (
   `id_transaksi` varchar(100) NOT NULL,
-  `id_pegawai` varchar(100) NOT NULL,
-  `id_barang_0` varchar(100) NOT NULL,
+  `id_barang_0` varchar(100) DEFAULT NULL,
   `id_barang_1` varchar(100) DEFAULT NULL,
   `id_barang_2` varchar(100) DEFAULT NULL,
   `id_barang_3` varchar(100) DEFAULT NULL,
@@ -120,16 +136,15 @@ CREATE TABLE `transaksi` (
   `id_barang_7` varchar(100) DEFAULT NULL,
   `id_barang_8` varchar(100) DEFAULT NULL,
   `id_barang_9` varchar(100) DEFAULT NULL,
-  `keterangan` varchar(100) NOT NULL,
-  `tanggal` datetime NOT NULL
+  `keterangan` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id_transaksi`, `id_pegawai`, `id_barang_0`, `id_barang_1`, `id_barang_2`, `id_barang_3`, `id_barang_4`, `id_barang_5`, `id_barang_6`, `id_barang_7`, `id_barang_8`, `id_barang_9`, `keterangan`, `tanggal`) VALUES
-('TRX_26888986', 'y', 'BARANG_12349853', 'BARANG_12349853', 'BARANG_29384387', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Kopi Kapal Api Hitam x2\nRental Pemancingan\n', '2024-03-26 04:36:10');
+INSERT INTO `transaksi` (`id_transaksi`, `id_barang_0`, `id_barang_1`, `id_barang_2`, `id_barang_3`, `id_barang_4`, `id_barang_5`, `id_barang_6`, `id_barang_7`, `id_barang_8`, `id_barang_9`, `keterangan`) VALUES
+('TRX_31603246', 'BRG_57654709', 'BRG_57654709', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Indomie Goreng x2\n');
 
 --
 -- Indexes for dumped tables
@@ -142,11 +157,19 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_stok`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
 -- Indexes for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  ADD PRIMARY KEY (`id_detail`),
-  ADD KEY `id_transaksi` (`id_transaksi`);
+  ADD PRIMARY KEY (`id_detail_transaksi`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
 -- Indexes for table `event`
@@ -165,7 +188,6 @@ ALTER TABLE `pegawai`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `id_pegawai` (`id_pegawai`),
   ADD KEY `id_barang_0` (`id_barang_0`),
   ADD KEY `id_barang_1` (`id_barang_1`),
   ADD KEY `id_barang_2` (`id_barang_2`),
@@ -182,26 +204,32 @@ ALTER TABLE `transaksi`
 --
 
 --
+-- Constraints for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD CONSTRAINT `barang_masuk_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
 -- Constraints for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`);
 
 --
 -- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`),
-  ADD CONSTRAINT `transaksi_ibfk_10` FOREIGN KEY (`id_barang_8`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_11` FOREIGN KEY (`id_barang_9`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_barang_0`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_barang_1`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_4` FOREIGN KEY (`id_barang_2`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_5` FOREIGN KEY (`id_barang_3`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_6` FOREIGN KEY (`id_barang_4`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_7` FOREIGN KEY (`id_barang_5`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_8` FOREIGN KEY (`id_barang_6`) REFERENCES `barang` (`id_barang`),
-  ADD CONSTRAINT `transaksi_ibfk_9` FOREIGN KEY (`id_barang_7`) REFERENCES `barang` (`id_barang`);
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_barang_0`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_10` FOREIGN KEY (`id_barang_9`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_barang_1`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_barang_2`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_4` FOREIGN KEY (`id_barang_3`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_5` FOREIGN KEY (`id_barang_4`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_6` FOREIGN KEY (`id_barang_5`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_7` FOREIGN KEY (`id_barang_6`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_8` FOREIGN KEY (`id_barang_7`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_ibfk_9` FOREIGN KEY (`id_barang_8`) REFERENCES `barang` (`id_barang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
